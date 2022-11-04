5644dd0e58950b4ba2c8be2eee0a0e517d35314c
Sergio <sergioa.orozcoo@gmail.com>
2022-11-04 17:19:53 -0500

    changes in UI of Login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,41 +1,22 @@
 <template>
   <div>
-    <main class="flex bg-gray-300 w-full">
-      <!-- Login -->
+    <main class="flex bg-slate-300">
+      <!-- Left side -->
       <div
-        class="lg:w-1/2 w-full flex flex-col justify-center items-center h-screen"
+        class="lg:w-1/2 w-full py-5 flex flex-col gap-3 justify-between items-center h-screen"
       >
+      <!-- Title Left Side Logo -->
+      <h1 class="px-6 text-slate-400 self-center font-bold text-3xl">SolarBoard</h1>
+      <!-- Login --> 
         <form
-          class="shadow-lg px-6 py-10 rounded-lg bg-white w-10/12 flex flex-col gap-3"
+          class="shadow-lg px-6 py-5 lg:py-10 rounded-lg self-center bg-white md:w-10/12 lg:w-7/12 w-4/5 flex flex-col gap-3"
         >
-          <h1 class="text-4xl text-center sm:text-left text-slate-800 font-bold mb-3">Welcome Back</h1>
-          <div class="flex flex-col md:flex-row gap-2">
-            <router-link to="/home" class="flex w-full md:w-1/2">
+          <h1 class="text-3xl text-center text-slate-800 font-bold mb-3">Welcome Back</h1>
+          <div class="flex flex-col lg:flex-row gap-2 w-full">
+            <router-link to="/home" class="flex w-full lg:w-1/2">
               <div
-                class="
-                  flex
-                  w-full
-                  h-7
-                  justify-center
-                  px-2
-                  py-5
-                  gap-2
-                  border-gray-200
-                  rounded-md
-                  border
-                  text-gray-500
-                  bg-white
-                  transition-all
-                  hover:bg-gray-200
-                  items-center
-                "
-              >
-                <svg
-                  class="w-5 h-5"
-                  viewBox="0 0 21 20"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
+                class="flex w-full h-7 justify-center px-1 py-5 gap-2 border-gray-200 rounded-md border text-gray-500 bg-white transition-all hover:bg-gray-200 items-center">
+                <svg class="w-5 h-5" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <g clip-path="url(#clip0_13183_10121)">
                     <path
                       d="M20.3081 10.2303C20.3081 9.55056 20.253 8.86711 20.1354 8.19836H10.7031V12.0492H16.1046C15.8804 13.2911 15.1602 14.3898 14.1057 15.0879V17.5866H17.3282C19.2205 15.8449 20.3081 13.2728 20.3081 10.2303Z"
@@ -56,39 +37,17 @@
                   </g>
                   <defs>
                     <clipPath id="clip0_13183_10121">
-                      <rect
-                        width="20"
-                        height="20"
-                        fill="white"
-                        transform="translate(0.5)"
-                      ></rect>
+                      <rect width="20" height="20" fill="white" transform="translate(0.5)"></rect>
                     </clipPath>
                   </defs>
                 </svg>
                 <p>Sign in with Google</p>
               </div>
             </router-link>
-            <router-link to="/home" class="flex w-full md:w-1/2">
+            <router-link to="/home" class="flex w-full lg:w-1/2">
               <div
-                class="
-                  flex
-                  w-full
-                  h-7
-                  justify-center
-                  px-2
-                  py-5
-                  gap-2
-                  border-gray-200
-                  rounded-md
-                  border
-                  text-gray-500
-                  bg-white
-                  transition-all
-                  hover:bg-gray-200
-                  items-center
-                "
-              >
-                <svg class="mr-2 -ml-1 w-5 h-5" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github"
+                class="flex w-full h-7 justify-center px-1 py-5 gap-2 border-gray-200 rounded-md border text-gray-500 bg-white transition-all hover:bg-gray-200 items-center">
+                <svg class="w-5 h-5" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github"
                   role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
                   <path fill="text-slate-800"
                     d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
@@ -98,18 +57,9 @@
             </router-link>
           </div>
           <div class="text-center inline-flex justify-center items-center w-full">
-            <hr class="my-8 w-64 h-px bg-gray-200 border-0 dark:bg-gray-700" />
+            <hr class="my-4 w-64 h-px bg-gray-200 border-0 dark:bg-gray-700" />
             <span
-              class="
-                absolute
-                px-3
-                font-medium
-                text-gray-900
-                bg-white
-                dark:text-white dark:bg-gray-900
-              "
-              >or</span
-            >
+              class="absolute px-3 font-medium text-gray-900 bg-white dark:text-white dark:bg-gray-900">or</span>
           </div>
           <!-- input Email -->
           <div class="flex flex-col">
@@ -128,19 +78,19 @@
             Sign in
           </button>
         </form>
+      <!-- Social Media -->
+          <a  target="_blank" class="flex gap-2 hover:underline" href="https://github.com/Sergioaorozco/SolarDashboard">
+            <p>Check out the Code</p>
+            <svg class="mr-2 -ml-1 w-5 h-5" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github"
+              role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+              <path fill="text-slate-800"
+                d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
+            </svg>
+          </a>
       </div>
       <!-- Picture -->
       <div
-        class="
-          bg-slate-800 bg-login-image
-          h-screen
-          lg:w-1/2
-          lg:flex
-          hidden
-          place-items-center
-          justify-center
-        "
-      >
+        class="bg-slate-800 bg-login-image h-screen lg:w-1/2 lg:flex hidden place-items-center justify-center">
         <figure>
           <img src="https://www.sergiorozco.com/images/Target.png" alt="" />
         </figure>

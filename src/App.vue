8b9668b655d3e6382c0e3df1752a6f0db70c6848
Sergio <sergioa.orozcoo@gmail.com>
2022-11-04 17:41:27 -0500

    Remove unused views within app

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Login from './views/Login.vue'
-import Home from './views/Home.vue'
 </script>
 
 <template>

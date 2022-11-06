63aa078a3275a3b8042f7fa4dbff35d0e2ce0308
Sergio <sergioa.orozcoo@gmail.com>
2022-11-05 22:24:54 -0500

    Add different error messages

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -64,7 +64,7 @@
           <!-- input Email -->
           <div class="flex flex-col">
             <label for="email">Email</label>
-            <input id="clientEmail" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="example@solar.com" type="email" name="emailValue" />
+            <input id="clientEmail" autocomplete="current-password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="example@solar.com" type="email" name="emailValue" />
           </div>
           <!-- Input Password -->
           <div class="flex flex-col">

94da62082a6b4fdab8e17bdbd600a9d27d83efaf
Sergio <sergioa.orozcoo@gmail.com>
2022-11-07 18:38:57 -0500

    Add image to logged users

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@
       <img src="" alt="">
     </figure>
     <h1>Welcomee {{userLogged.displayName}}</h1>
+    <img class="w-12 h-12" :src="userLogged.photoURL" alt="">
   </div>
 </template>
 <script>

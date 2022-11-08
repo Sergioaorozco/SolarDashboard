f6e07b1acbe38e9a3a75f6d41d0ccee9e7f2f8cb
Sergio <sergioa.orozcoo@gmail.com>
2022-11-08 14:59:25 -0500

    Add Register page

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -22,7 +22,7 @@
                 </div>
                 <div class="mb-6">
                     <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Phone number</label>
-                    <input type="tel" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123-45-678" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required>
+                    <input type="tel" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123-45-678" pattern="[0-9]{10}" required>
                 </div>
                 <div class="mb-6">
                     <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email address</label>
@@ -87,10 +87,16 @@ export default {
     EmailSignUp(e){
       const email = document.getElementById('email').value
       const password = document.getElementById('password').value
+      const firstName = document.getElementById('first_name').value
+      const lastName = document.getElementById('last_name').value
+      const phoneNumber = document.getElementById('phone').value
+      const updateUser = auth.currentUser
       e.preventDefault()
       createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
           const user = userCredential.user
+          updateUser.displayName= `${firstName} + ${lastName}`
+          updateUser.phoneNumber= phoneNumber
           console.log(user)
           console.log('User Created')
           router.push('/')

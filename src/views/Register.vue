0358f2453348f3e1ef074c0a958af7efec9ecd7a
Sergio <sergioa.orozcoo@gmail.com>
2022-11-08 10:31:24 -0500

    Register Submit method

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -42,7 +42,7 @@
                     </div>
                     <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-400">I agree with the <a href="#" class="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a>.</label>
                 </div>
-                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create Account</button>
+                <button @submit="EmailSign" type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create Account</button>
             </form>
         </div>
         <router-link to="/" class="text-white  justify-center self-center underline" href="">Already have an account? Login here</router-link>

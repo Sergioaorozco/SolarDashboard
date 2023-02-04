8f56283cde9cffa3676704c92352feb26130c8e5
Sergio <sergioa.orozcoo@gmail.com>
2023-02-04 15:53:33 -0500

    Adding Styles to Header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
       <section class="bg-slate-200">
         <header class="wrapper m-auto py-2">
           <div class="flex flex-col lg:justify-between lg:flex-row">
-            <div class="flex">
+            <div class="flex gap-1">
               <img class="w-12 h-12 rounded-full" :src="userLogged.photoURL" alt="profile image">
               <div>
                 <h1 class=" font-bold text-lg">Welcome</h1>

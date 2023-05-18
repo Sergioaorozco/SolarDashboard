12196603108acb7ec8f5abc1bd4d9137aaedf69b
Sergio <sergioa.orozcoo@gmail.com>
2023-05-18 13:40:14 -0500

    Add Tasks board

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,33 +1,112 @@
 <template>
-    <div>
-      <p class="text-slate-600 text-2xl font-bold mb-3">Tasks</p>
-      <div class="flex items-center gap-x-3">
-        <MultiSelect v-model="selectedCities" :options="cities" filter optionLabel="name" placeholder="Select Cities"
-          :maxSelectedLabels="3" class="w-full md:w-20rem" />
-        <button class="bg-blue-600 text-white rounded-md px-5 py-3 whitespace-pre hover:bg-blue-800 transition-colors duration-300">Agregar Proyecto</button>
-      </div>
+  <div>
+    <p class="text-slate-600 text-2xl font-bold mb-3">Tasks</p>
+    <div class="flex items-center gap-x-3">
+      <Dropdown v-model="selectedProject" :options="projects" filter optionLabel="name" placeholder="Select a Project"
+        class="w-full md:w-14rem">
+        <template #value="slotProps">
+          <div v-if="slotProps.value" class="flex align-items-center">
+            <Chip>{{ slotProps.value.name }}</Chip>
+          </div>
+          <span v-else>
+            {{ slotProps.placeholder }}
+          </span>
+        </template>
+        <template #option="slotProps">
+          <div class="flex align-items-center">
+            <img :alt="slotProps.option.label" src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png"
+              :class="`mr-2 flag flag-${slotProps.option.code.toLowerCase()}`" style="width: 18px" />
+            <div>{{ slotProps.option.name }}</div>
+          </div>
+        </template>
+      </Dropdown>
+      <button
+        class="bg-blue-600 text-white rounded-md px-5 py-3 w-56 hover:bg-blue-800 transition-colors duration-300">Agregar
+        Proyecto</button>
     </div>
+    <section class="grid grid-cols-4 mt-6 gap-x-4">
+      <div v-for="status in board" class="h-full">
+        <article class="flex flex-col justify-center items-centers gap-y-4 pb-6">
+          <div :class="status.color" class="card text-slate-600 font-semibold">{{ status.name }}</div>
+          <div :class="status.color" class="card h-auto flex flex-col gap-y-4 border-none">
+            <!-- Each of one the cards -->
+            <div class="card bg-white h-auto px-4 inline-block">
+              <div class="flex justify-between items-center mb-4">
+                <p class="text-xs bg-slate-100 px-4 py-1 rounded-md text-slate-600">Contact Management</p>
+                <span class="w-6 h-6 text-[0.7rem] bg-white border border-slate-200 rounded-full flex justify-center items-center hover:bg-slate-200 transition-colors duration-300"><i class="pi pi-ellipsis-v"></i></span>
+              </div>
+              <h3 class="text-slate-500 font-semibold mb-2">Tarjeta 1</h3>
+              <p class="text-slate-400 text-sm">A "Not Started" card refers to a task or project that has not yet been initiated or commenced. It representation of the task or project...</p>
+            </div>
+            <!-- Each of one the cards -->
+            <div class="card bg-white h-auto px-4 inline-block">
+              <div class="flex justify-between items-center mb-4">
+                <p class="text-xs bg-slate-100 px-4 py-1 rounded-md text-slate-600">Calltracking</p>
+                <span class="w-6 h-6 text-[0.7rem] bg-white border border-slate-200 rounded-full flex justify-center items-center hover:bg-slate-200 transition-colors duration-300"><i class="pi pi-ellipsis-v"></i></span>
+              </div>
+              <h3 class="text-slate-500 font-semibold mb-2">Tarjeta 1</h3>
+              <p class="text-slate-400 text-sm">A "Not Started" card refers to a task or project that has not yet been initiated or commenced. It representation of the task or project...</p>
+            </div>
+            <!-- Each of one the cards -->
+            <div class="card bg-white h-auto px-4 inline-block">
+              <div class="flex justify-between items-center mb-4">
+                <p class="text-xs bg-slate-100 px-4 py-1 rounded-md text-slate-600">Funds</p>
+                <span class="w-6 h-6 text-[0.7rem] bg-white border border-slate-200 rounded-full flex justify-center items-center hover:bg-slate-200 transition-colors duration-300"><i class="pi pi-ellipsis-v"></i></span>
+              </div>
+              <h3 class="text-slate-500 font-semibold mb-2">Tarjeta 1</h3>
+              <p class="text-slate-400 text-sm">A "Not Started" card refers to a task or project that has not yet been initiated or commenced. It representation of the task or project...</p>
+            </div>
+            <!-- Each of one the cards -->
+            <div class="card bg-white h-auto px-4 inline-block">
+              <div class="flex justify-between items-center mb-4">
+                <p class="text-xs bg-slate-100 px-4 py-1 rounded-md text-slate-600">Funds</p>
+                <span class="w-6 h-6 text-[0.7rem] bg-white border border-slate-200 rounded-full flex justify-center items-center hover:bg-slate-200 transition-colors duration-300"><i class="pi pi-ellipsis-v"></i></span>
+              </div>
+              <h3 class="text-slate-500 font-semibold mb-2">Tarjeta 1</h3>
+              <p class="text-slate-400 text-sm">A "Not Started" card refers to a task or project that has not yet been initiated or commenced. It representation of the task or project...</p>
+            </div>
+          </div>
+        </article>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-import MultiSelect from 'primevue/multiselect';
+import Dropdown from 'primevue/dropdown';
+import Chip from 'primevue/Chip';
+import Tag from 'primevue/tag';
+
+
 
 export default {
   data() {
     return {
-      selectedCities: '',
-      cities: [
-        { name: 'New York', code: 'NY' },
-        { name: 'Rome', code: 'RM' },
-        { name: 'London', code: 'LDN' },
+      selectedProject: '',
+      board: [
+        { name: 'Not Started', color:'bg-stale-100' },
+        { name: 'In progress', color:'bg-stale-100' },
+        { name: 'Ready for QA', color:'bg-stale-100' },
+        { name: 'Done', color:'bg-green-50' },
+      ],
+      projects: [
+        { name: 'Newsletter May 2023', code: 'RM' },
+        { name: 'Print and Distribution', code: 'LDN' },
+        { name: 'Project Vue 1', code: 'NY' },
         { name: 'Istanbul', code: 'IST' },
-        { name: 'Paris', code: 'PRS' }        
+        { name: 'Speech preparation', code: 'PRS' }
       ]
     }
   },
   components: {
-    MultiSelect,
-
+    Dropdown,
+    Chip,
+    Tag,
+  },
+  methods: {
+    addToproject(e){
+      console.log(e);
+    }
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
 import App from './App.vue'
 import router from './router'
 
 // Styles
 import './style.scss'
-import "primevue/resources/themes/lara-light-indigo/theme.css";     
+import 'primevue/resources/themes/lara-light-blue/theme.css'
 import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
 
@@ -18,6 +19,7 @@ const pinia = createPinia();
 const vue = createApp(App);
 vue
 .directive('tooltip', Tooltip)
+.use(PrimeVue)
 .use(pinia)
 .use(router)
 .mount('#app');

469f9f1144761dbbdb6c25391dbaa97816a9967e
Sergio <sergioa.orozcoo@gmail.com>
2023-05-19 11:09:07 -0500

    Update Card information

---
+++ src/components/CardTask.vue
@@ -0,0 +1,34 @@
+<template>
+    <!-- Each of one the cards -->
+    <div class="card bg-white h-auto px-4 inline-block">
+      <div class="flex justify-between items-center mb-4">
+        <p class="text-xs bg-slate-100 px-4 py-1 rounded-md text-slate-600">{{cardInfo.tag}}</p>
+        <span
+          class="actionsMenu w-6 h-6 text-[0.7rem] bg-white border border-slate-200 rounded-full flex justify-center items-center hover:bg-slate-200 transition-colors duration-300"><i
+            class="pi pi-ellipsis-v"></i></span>
+      </div>
+      <h3 class="text-slate-500 font-semibold mb-2">{{cardInfo.title}}</h3>
+      <p class="text-slate-400 text-sm mb-2">{{ cardInfo.description }}</p>
+        <ProgressBar :value="cardInfo.value" style="height: 6px;"/>
+    </div>
+</template>
+
+<script>
+import ProgressBar from 'primevue/progressbar';
+
+export default {
+  data(){
+    return {
+    }
+  },
+  components: {
+    ProgressBar
+  },
+  props: ['cardInfo']
+}
+
+</script>
+
+<style scoped>
+  ::v-deep .p-progressbar-label { display: none; }
+</style>
--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="">
     <p class="text-slate-600 text-2xl font-bold mb-3">Tasks</p>
     <div class="flex items-center gap-x-3">
       <Dropdown v-model="selectedProject" :options="projects" filter optionLabel="name" placeholder="Select a Project"
@@ -28,46 +28,14 @@
       <div v-for="status in board" class="h-full">
         <article class="flex flex-col justify-center items-centers gap-y-4 pb-6">
           <div :class="status.color" class="card text-slate-600 font-semibold">{{ status.name }}</div>
-          <div :class="status.color" class="card h-auto flex flex-col gap-y-4 border-none">
-            <!-- Each of one the cards -->
-            <div class="card bg-white h-auto px-4 inline-block">
-              <div class="flex justify-between items-center mb-4">
-                <p class="text-xs bg-slate-100 px-4 py-1 rounded-md text-slate-600">Contact Management</p>
-                <span class="actionsMenu w-6 h-6 text-[0.7rem] bg-white border border-slate-200 rounded-full flex justify-center items-center hover:bg-slate-200 transition-colors duration-300"><i class="pi pi-ellipsis-v"></i></span>
-              </div>
-              <h3 class="text-slate-500 font-semibold mb-2">Tarjeta 1</h3>
-              <p class="text-slate-400 text-sm">A "Not Started" card refers to a task or project that has not yet been initiated or commenced. It representation of the task or project...</p>
-            </div>
-            <!-- Each of one the cards -->
-            <div class="card bg-white h-auto px-4 inline-block">
-              <div class="flex justify-between items-center mb-4">
-                <p class="text-xs bg-slate-100 px-4 py-1 rounded-md text-slate-600">Calltracking</p>
-                <span class="actionsMenu w-6 h-6 text-[0.7rem] bg-white border border-slate-200 rounded-full flex justify-center items-center hover:bg-slate-200 transition-colors duration-300"><i class="pi pi-ellipsis-v"></i></span>
-              </div>
-              <h3 class="text-slate-500 font-semibold mb-2">Tarjeta 1</h3>
-              <p class="text-slate-400 text-sm">A "Not Started" card refers to a task or project that has not yet been initiated or commenced. It representation of the task or project...</p>
-            </div>
-            <!-- Each of one the cards -->
-            <div class="card bg-white h-auto px-4 inline-block">
-              <div class="flex justify-between items-center mb-4">
-                <p class="text-xs bg-slate-100 px-4 py-1 rounded-md text-slate-600">Funds</p>
-                <span class="actionsMenu w-6 h-6 text-[0.7rem] bg-white border border-slate-200 rounded-full flex justify-center items-center hover:bg-slate-200 transition-colors duration-300"><i class="pi pi-ellipsis-v"></i></span>
-              </div>
-              <h3 class="text-slate-500 font-semibold mb-2">Tarjeta 1</h3>
-              <p class="text-slate-400 text-sm">A "Not Started" card refers to a task or project that has not yet been initiated or commenced. It representation of the task or project...</p>
-            </div>
-            <!-- Each of one the cards -->
-            <div class="card bg-white h-auto px-4 inline-block">
-              <div class="flex justify-between items-center mb-4">
-                <p class="text-xs bg-slate-100 px-4 py-1 rounded-md text-slate-600">Funds</p>
-                <span class="actionsMenu w-6 h-6 text-[0.7rem] bg-white border border-slate-200 rounded-full flex justify-center items-center hover:bg-slate-200 transition-colors duration-300"><i class="pi pi-ellipsis-v"></i></span>
-              </div>
-              <h3 class="text-slate-500 font-semibold mb-2">Tarjeta 1</h3>
-              <p class="text-slate-400 text-sm">A "Not Started" card refers to a task or project that has not yet been initiated or commenced. It representation of the task or project...</p>
+          <div class="bg-transparent card h-auto flex flex-col gap-y-4 border-none">
+            <div v-for="card in cardInformation" >
+              <CardTask v-if="card.status === status.name.toLowerCase().replace(' ', '-')" :cardInfo="card" />
             </div>
           </div>
         </article>
       </div>
+      
     </section>
   </div>
 </template>
@@ -75,16 +43,17 @@
 <script>
 import Dropdown from 'primevue/dropdown';
 import Chip from 'primevue/chip';
+import CardTask from '../components/CardTask.vue'
 
 export default {
   data() {
     return {
       selectedProject: '',
       board: [
-        { name: 'Not Started', color:'bg-stale-100' },
-        { name: 'In progress', color:'bg-stale-100' },
-        { name: 'Ready for QA', color:'bg-stale-100' },
-        { name: 'Done', color:'bg-green-50' },
+        { name: 'Not Started', color:'bg-slate-100', cardStatus: true,  },
+        { name: 'In progress', color:'bg-orange-50', cardStatus: false },
+        { name: 'Ready for QA', color:'bg-sky-50', cardStatus: false  },
+        { name: 'Done', color:'bg-lime-50', cardStatus: true  },
       ],
       projects: [
         { name: 'Newsletter May 2023', code: 'RM' },
@@ -92,12 +61,18 @@ export default {
         { name: 'Project Vue 1', code: 'NY' },
         { name: 'Istanbul', code: 'IST' },
         { name: 'Speech preparation', code: 'PRS' }
+      ],
+      cardInformation: [
+        {title:'Tarjeta 1', tag: 'Contact', description: 'A "Not Started" card refers to a task or project that has not yet been initiated or commenced. It representation of the task or project...', value: 30, status: 'not-started'},
+        {title:'Tarjeta 2 Completada', tag: 'CallTracking', description: 'A "Done" card refers to a task or project that has been finalied. It representation of the task or project...', value: 100, status: 'done'},   
+        {title:'Tarjeta 3', tag: 'CallTracking', description: 'A "Done" card refers to a task or project that has been finalied. It representation of the task or project...', value: 21, status: 'not-started'},   
       ]
     }
   },
   components: {
     Dropdown,
     Chip,
+    CardTask
   },
   methods: {
     addToproject(e){
